71022b37dba36acf95b468930903617dc85b3e71
honey720 <honey720@kyonggi.ac.kr>
2025-11-20 17:50:22 +0900

    feat: 결제 도메인 구현

---
+++ src/main/java/com/example/shop/payment/client/TossPaymentClient.java
@@ -0,0 +1,55 @@
+package com.example.shop.payment.client;
+
+import com.example.shop.payment.application.dto.PaymentCommand;
+import com.example.shop.payment.client.dto.TossPaymentResponse;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class TossPaymentClient {
+
+    private static final String CONFIRM_URL = "https://api.tosspayments.com/v1/payments/confirm";
+
+    private final RestTemplate restTemplate;
+    @Value("${payment.toss.secret-key}")
+    private String secretKey;
+
+    public TossPaymentResponse confirm(PaymentCommand command) throws HttpStatusCodeException {
+        if (secretKey == null) {
+            throw new IllegalStateException("Toss secret key is not configured");
+        }
+        //토스에 요청할 header
+        HttpHeaders headers = createHeaders();
+        Map<String, Object> body = new HashMap<>();
+        body.put("paymentKey", command.paymentKey());
+        body.put("orderId", command.orderId());
+        body.put("amount", command.amount());
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, headers);
+        return restTemplate.postForObject(CONFIRM_URL, entity, TossPaymentResponse.class);
+    }
+
+    private HttpHeaders createHeaders() { //토스에 전달하는 header 값 생성
+        HttpHeaders headers = new HttpHeaders(); //headers 생성
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        String auth = secretKey + ":";
+        //secretkey base64 Encode
+        String encoded = Base64.getEncoder().encodeToString(auth.getBytes(StandardCharsets.UTF_8));
+        //Authorization header에 인코딩 값 입력.
+        headers.set(HttpHeaders.AUTHORIZATION, "Basic " + encoded);
+        return headers;
+    }
+}
