e92e166286a5ff8d79134de40dad60dad0bd9352
honey720 <honey720@kyonggi.ac.kr>
2025-11-20 12:48:59 +0900

    feat: 판매자 페이지 도메인 구현

---
+++ src/main/java/com/example/shop/seller/application/dto/SellerCommand.java
@@ -0,0 +1,11 @@
+package com.example.shop.seller.application.dto;
+
+public record SellerCommand(String companyName,
+                            String representativeName,
+                            String email,
+                            String phone,
+                            String businessNumber,
+                            String address,
+                            String status
+) {
+}
